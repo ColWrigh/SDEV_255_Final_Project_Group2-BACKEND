2d5302dcba1478010a6ab0a3e39a8279d0399a53
CodyG0228 <codyrg0228@gmail.com>
2026-07-20 12:34:08 -0500

    "dbinit.js" initialized. this initializes database with default values, then exits

---
+++ scripts/dbinit.js
@@ -0,0 +1,12 @@
+const db = require('./database'); //Requiring the already existing database.js file. Establishes connection.
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS User (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_name TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        role TEXT CHECK(role IN ('student', 'teacher')) NOT NULL
+    );
+`);
+
+//backticks above allowing many multi-line strings to be used at once 
