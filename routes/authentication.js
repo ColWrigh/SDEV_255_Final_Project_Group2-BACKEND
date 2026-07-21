ea8e65cd578515e54e4f26906a5eeb4ed4bda705
CodyG0228 <codyrg0228@gmail.com>
2026-07-20 19:57:51 -0500

    added "next" passes and commented out. unsure if it will cause errors in real-time.

--- routes/authentication.js
+++ routes/authentication.js
@@ -6,16 +6,19 @@ const { authToken, requireStudent, requireTeacher } = require('../middleware/aut
 // this route is for teachers ONLY
 router.get("/teacherPanel", verifyToken, requireTeacher, (req, res) => {
     res.json({ message: "Welcome, teacher!" });
+    //next();
 });
 
 // this route is for students ONLY
 router.get("/studentPanel", verifyToken, requireStudent, (req, res) => {
     res.json({ message: "Welcome, student!" });
+    //next();
 });
 
 // and finally this route is for any authenticated user
 router.get("/profile", verifyToken, (req, res) => {
     res.json({ user: req.user });
+    //next();  commented out because I don't *believe* we need this, but unsure in practice
 });
 
 module.exports = router;
