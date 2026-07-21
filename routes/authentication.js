ac6be070d390aca97bbec80be7204e263f8a654a
CodyG0228 <codyrg0228@gmail.com>
2026-07-20 20:01:55 -0500

    made a mistake in routes/authentication.js, fixed.

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,19 +4,19 @@ const router = express.Router(); //router to handle requests
 const { authToken, requireStudent, requireTeacher } = require('../middleware/authentication'); //this imports the three functions from /middleware/authentication.js
 
 // this route is for teachers ONLY
-router.get("/teacherPanel", verifyToken, requireTeacher, (req, res) => {
+router.get("/teacherPanel", authToken, requireTeacher, (req, res) => {
     res.json({ message: "Welcome, teacher!" });
     //next();
 });
 
 // this route is for students ONLY
-router.get("/studentPanel", verifyToken, requireStudent, (req, res) => {
+router.get("/studentPanel", authToken, requireStudent, (req, res) => {
     res.json({ message: "Welcome, student!" });
     //next();
 });
 
 // and finally this route is for any authenticated user
-router.get("/profile", verifyToken, (req, res) => {
+router.get("/profile", authToken, (req, res) => {
     res.json({ user: req.user });
     //next();  commented out because I don't *believe* we need this, but unsure in practice
 });
